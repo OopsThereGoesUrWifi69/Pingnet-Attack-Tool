783030bf9282e5442dc6dd66221294c485050793
OopsThereGoesUrWifi69 <60832386+OopsThereGoesUrWifi69@users.noreply.github.com>
2020-05-12 16:14:51 -0400

    Add files via upload
    [ONLY FOR MAC OS AT THE MOMENT I AM WORKING ON A WINDOWS VERSION AS WELL] cd to where the file is 
    and then type "perl pythoninstall.c" and it will install python3

---
+++ pythoninstall.c
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+import sys
+
+os.system("$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
+os.system("clear")
+os.system("brew install python3")
